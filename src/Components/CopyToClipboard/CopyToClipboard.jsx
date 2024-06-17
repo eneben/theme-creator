a27e604f43bbea8de1fe1667092a3e4d443b603e
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-06-17 11:10:36 +0200

    add button and import in color.jsx

--- src/Components/CopyToClipboard/CopyToClipboard.jsx
+++ src/Components/CopyToClipboard/CopyToClipboard.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Button from "../Button/Button";
 
 export default function CopyToClipboard({ copiedText }) {
   const [isCopied, setIsCopied] = useState(false);
@@ -17,5 +18,14 @@ export default function CopyToClipboard({ copiedText }) {
     const timer = setInterval(() => {
       setIsCopied(false);
     }, 3000);
+    return () => {
+      clearInterval(timer);
+    };
   }, [isCopied]);
+
+  <Button
+    type="button"
+    onClick={writeClipboardText}
+    text={isCopied ? "SUCCESFULLY COPIED!" : "COPY"}
+  />;
 }
