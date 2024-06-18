97d478b02db95df30d572ade4c34dea9a6e9f05e
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-06-18 11:47:30 +0200

    add styling to contrast score based on the result

--- src/Components/ContrastChecker/ContrastChecker.jsx
+++ src/Components/ContrastChecker/ContrastChecker.jsx
@@ -1,44 +1,55 @@
 import { useEffect, useState } from "react";
+import "./ContrastChecker.css";
 
 export default function ContrastChecker({ firstColor, secondColor }) {
   const [contrastResult, setContrastResult] = useState(null);
 
-  async function fetchContrast() {
-    try {
-      const response = await fetch(
-        "https://www.aremycolorsaccessible.com/api/are-they",
-        {
-          method: "POST",
-          mode: "cors",
-          body: JSON.stringify({ colors: [firstColor, secondColor] }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error(
-          `Failed to fetch data! Status Code: ${response.status}`
-        );
-      }
-
-      const contrastData = await response.json();
-      setContrastResult(contrastData.overall);
-
-      console.log("contrastData", contrastData);
-      console.log("contrastResult", contrastResult);
-    } catch (error) {
-      alert(`An error occurred: ${error.message}`);
-    }
-  }
-
   useEffect(() => {
+    async function fetchContrast() {
+      try {
+        const response = await fetch(
+          "https://www.aremycolorsaccessible.com/api/are-they",
+          {
+            method: "POST",
+            mode: "cors",
+            body: JSON.stringify({ colors: [firstColor, secondColor] }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(
+            `Failed to fetch data! Status Code: ${response.status}`
+          );
+        }
+
+        const contrastData = await response.json();
+        setContrastResult(contrastData.overall);
+
+        console.log("contrastData", contrastData);
+        console.log("contrastResult", contrastResult);
+      } catch (error) {
+        alert(`An error occurred: ${error.message}`);
+      }
+    }
+
     fetchContrast();
   }, [firstColor, secondColor]);
 
   return (
-    <p className="contrastScore">
+    <p
+      className={`contrastResult ${
+        contrastResult === "Yup"
+          ? "contrastYup"
+          : contrastResult === "Kinda"
+          ? "contrastKinda"
+          : contrastResult === "Nope"
+          ? "contrastNope"
+          : "contrastDefault"
+      }`}
+    >
       Overall Contrast Score: {contrastResult ? contrastResult : "loading..."}
     </p>
   );
