73929227e0b037127a5a9968814b3147db9143c6
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-06-18 14:02:59 +0200

    remove console.logs and parameter in useEffect

--- src/Components/ContrastChecker/ContrastChecker.jsx
+++ src/Components/ContrastChecker/ContrastChecker.jsx
@@ -4,42 +4,36 @@ import "./ContrastChecker.css";
 export default function ContrastChecker({ firstColor, secondColor }) {
   const [contrastResult, setContrastResult] = useState(null);
 
-  useEffect(
-    (contrastResult) => {
-      async function fetchContrast() {
-        try {
-          const response = await fetch(
-            "https://www.aremycolorsaccessible.com/api/are-they",
-            {
-              method: "POST",
-              mode: "cors",
-              body: JSON.stringify({ colors: [firstColor, secondColor] }),
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
-
-          if (!response.ok) {
-            throw new Error(
-              `Failed to fetch data! Status Code: ${response.status}`
-            );
+  useEffect(() => {
+    async function fetchContrast() {
+      try {
+        const response = await fetch(
+          "https://www.aremycolorsaccessible.com/api/are-they",
+          {
+            method: "POST",
+            mode: "cors",
+            body: JSON.stringify({ colors: [firstColor, secondColor] }),
+            headers: {
+              "Content-Type": "application/json",
+            },
           }
+        );
 
-          const contrastData = await response.json();
-          setContrastResult(contrastData.overall);
-
-          console.log("contrastData", contrastData);
-          console.log("contrastResult", contrastResult);
-        } catch (error) {
-          alert(`An error occurred: ${error.message}`);
+        if (!response.ok) {
+          throw new Error(
+            `Failed to fetch data! Status Code: ${response.status}`
+          );
         }
-      }
 
-      fetchContrast();
-    },
-    [firstColor, secondColor]
-  );
+        const contrastData = await response.json();
+        setContrastResult(contrastData.overall);
+      } catch (error) {
+        alert(`An error occurred: ${error.message}`);
+      }
+    }
+
+    fetchContrast();
+  }, [firstColor, secondColor]);
 
   return (
     <p
