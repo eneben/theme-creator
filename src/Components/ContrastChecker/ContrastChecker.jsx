25fd010090292fc82dd8269b10db70115ffd9e5c
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-06-18 12:08:33 +0200

    fix error: pass usestate constant to the useeffect function

--- src/Components/ContrastChecker/ContrastChecker.jsx
+++ src/Components/ContrastChecker/ContrastChecker.jsx
@@ -4,39 +4,42 @@ import "./ContrastChecker.css";
 export default function ContrastChecker({ firstColor, secondColor }) {
   const [contrastResult, setContrastResult] = useState(null);
 
-  useEffect(() => {
-    async function fetchContrast() {
-      try {
-        const response = await fetch(
-          "https://www.aremycolorsaccessible.com/api/are-they",
-          {
-            method: "POST",
-            mode: "cors",
-            body: JSON.stringify({ colors: [firstColor, secondColor] }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error(
-            `Failed to fetch data! Status Code: ${response.status}`
+  useEffect(
+    (contrastResult) => {
+      async function fetchContrast() {
+        try {
+          const response = await fetch(
+            "https://www.aremycolorsaccessible.com/api/are-they",
+            {
+              method: "POST",
+              mode: "cors",
+              body: JSON.stringify({ colors: [firstColor, secondColor] }),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
           );
+
+          if (!response.ok) {
+            throw new Error(
+              `Failed to fetch data! Status Code: ${response.status}`
+            );
+          }
+
+          const contrastData = await response.json();
+          setContrastResult(contrastData.overall);
+
+          console.log("contrastData", contrastData);
+          console.log("contrastResult", contrastResult);
+        } catch (error) {
+          alert(`An error occurred: ${error.message}`);
         }
-
-        const contrastData = await response.json();
-        setContrastResult(contrastData.overall);
-
-        console.log("contrastData", contrastData);
-        console.log("contrastResult", contrastResult);
-      } catch (error) {
-        alert(`An error occurred: ${error.message}`);
       }
-    }
 
-    fetchContrast();
-  }, [firstColor, secondColor]);
+      fetchContrast();
+    },
+    [firstColor, secondColor]
+  );
 
   return (
     <p
